84328d5672d123771c5ca36ef53f60d3ce300f8e
Pavel Korolev <dev@borodust.org>
2018-08-07 16:18:14 +0300

    Export color and tex-coord channels, tangents

--- scene.lisp
+++ scene.lisp
@@ -1,6 +1,10 @@
 (cl:in-package :bodge-converter)
 
 
+(define-constant +max-tex-coord-channels+ 8)
+(define-constant +max-color-channels+ 8)
+
+
 (declaim (special *scene*
                   *id*
                   *images*
@@ -64,7 +68,7 @@
 ;;;
 ;;; MESHES
 ;;;
-(defun make-attribute-array (source-ptr foreign-type vertex-count element-size)
+(defun make-attribute-array-from-continuous (source-ptr foreign-type vertex-count element-size)
   (let* ((length (* vertex-count element-size))
          (type (ecase foreign-type
                  (:float 'single-float)
@@ -73,6 +77,37 @@
          (dst-ptr (static-vector-pointer array)))
     (claw:memcpy dst-ptr source-ptr :n length :type foreign-type)
     array))
+
+
+(defun make-attribute-array-from-strided (source-ptr foreign-type vertex-count element-size
+                                          skipped-element-count)
+  ;; We heard you like C so we added a bit of it into your CL
+  (let* ((length (* vertex-count element-size))
+         (type (ecase foreign-type
+                 (:float 'single-float)
+                 (:unsigned-int '(unsigned-byte 32))))
+         (array (make-static-vector length :element-type type))
+         (dst-ptr (static-vector-pointer array))
+         (src-element-byte-size (* (claw:sizeof foreign-type) (+ element-size skipped-element-count)))
+         (dst-element-byte-size (* (claw:sizeof foreign-type) element-size))
+         (src-addr (cffi:pointer-address source-ptr))
+         (dst-addr (cffi:pointer-address dst-ptr)))
+    (loop for i below vertex-count
+          for from-addr = (+ src-addr (* src-element-byte-size i))
+          for to-addr = (+ dst-addr (* dst-element-byte-size i))
+          do (claw:memcpy (cffi:make-pointer to-addr)
+                          (cffi:make-pointer from-addr)
+                          :n element-size
+                          :type foreign-type))
+    array))
+
+
+(defun make-attribute-array (source-ptr foreign-type vertex-count element-size
+                             &optional (skipped-element-count 0))
+  (if (zerop skipped-element-count)
+      (make-attribute-array-from-continuous source-ptr foreign-type vertex-count element-size)
+      (make-attribute-array-from-strided source-ptr foreign-type vertex-count element-size
+                                         skipped-element-count)))
 
 
 (defun set-position-array (bodge-mesh mesh)
@@ -117,17 +152,57 @@
           (setf (ge.rsc:mesh-resource-normal-array bodge-mesh) array))))))
 
 
+(defun set-tangent-array (bodge-mesh mesh)
+  (with-mesh (mesh)
+    (let ((tangent-count (mesh :num-vertices)))
+      (when (> tangent-count 0)
+        (let ((array (make-attribute-array (mesh :tangents) :float tangent-count 3)))
+          (setf (ge.rsc:mesh-resource-tangent-array bodge-mesh) array))))))
+
+
+(defun set-tex-coord-arrays (bodge-mesh mesh)
+  (with-mesh (mesh)
+    (loop with tex-coord-count = (mesh :num-vertices)
+          for i below +max-tex-coord-channels+
+          for tex-coord-array-ptr = (mesh :texture-coords i)
+          for component-count = (mesh :num-uv-components i)
+          when (> component-count 0)
+            do (let ((attrib-array (make-attribute-array tex-coord-array-ptr
+                                                         :float
+                                                         tex-coord-count
+                                                         component-count
+                                                         (- 3 component-count))))
+                 (setf (ge.rsc:mesh-resource-tex-coord-array bodge-mesh i) attrib-array)))))
+
+
+(defun set-color-arrays (bodge-mesh mesh)
+  (with-mesh (mesh)
+    (loop with color-count = (mesh :num-vertices)
+          and component-count = 4
+          for i below +max-color-channels+
+          for color-array-ptr = (mesh :colors i)
+          unless (claw:null-pointer-p color-array-ptr)
+            do (let ((attrib-array (make-attribute-array color-array-ptr
+                                                         :float
+                                                         color-count
+                                                         component-count)))
+                 (setf (ge.rsc:mesh-resource-color-array bodge-mesh i) attrib-array)))))
+
+
 (defun make-mesh (mesh)
   (with-mesh (mesh)
     (let* ((primitive-type (switch ((mesh :primitive-types) :test #'=)
-                            (%ai:+primitive-type-point+ :points)
-                            (%ai:+primitive-type-line+ :lines)
-                            (%ai:+primitive-type-triangle+ :triangles)
-                            (t (error "Unexpected primitive type: ~A" (mesh :primitive-types)))))
+                             (%ai:+primitive-type-point+ :points)
+                             (%ai:+primitive-type-line+ :lines)
+                             (%ai:+primitive-type-triangle+ :triangles)
+                             (t (error "Unexpected primitive type: ~A" (mesh :primitive-types)))))
            (bodge-mesh (ge.rsc:make-mesh-resource primitive-type)))
       (set-position-array bodge-mesh mesh)
       (set-index-array bodge-mesh mesh)
       (set-normal-array bodge-mesh mesh)
+      (set-tangent-array bodge-mesh mesh)
+      (set-color-arrays bodge-mesh mesh)
+      (set-tex-coord-arrays bodge-mesh mesh)
       bodge-mesh)))
 
 
